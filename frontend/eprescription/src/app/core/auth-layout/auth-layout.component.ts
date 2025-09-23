8ef9203c3c7d8370ebb16d11cae47e3428108dff
Anis <anisictiu@gmail.com>
2025-09-23 20:16:13 +0600

    wu

--- frontend/eprescription/src/app/core/auth-layout/auth-layout.component.ts
+++ frontend/eprescription/src/app/core/auth-layout/auth-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MaterialModule } from '../../shared/material.module';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet,MaterialModule,CommonModule],
+  imports: [RouterOutlet, MaterialModule, CommonModule, RouterLink],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
